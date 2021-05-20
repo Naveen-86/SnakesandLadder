865bb683f57a0ebae84940fb27e496055dbec7f2
unknown <naveentruly86@gmail.com>
2021-05-20 17:14:52 +0530

    Added: Rolling the Dice

--- src/main/java/com/game/SnakenLadder.java
+++ src/main/java/com/game/SnakenLadder.java
@@ -1,10 +1,21 @@
 package com.game;
 
+import java.util.Random;
+
 public class SnakenLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snakes and ladders game");
 
         final int START_POSITION = 0;
         System.out.println("You are at position : " + START_POSITION);
+
+        System.out.println("Rolling Dice" );
+
+        Random dice_Roll = new Random();
+
+        int position = dice_Roll.nextInt(6)+1;
+
+        System.out.println("After Rolling dice You are at position : " + position);
+
     }
 }
