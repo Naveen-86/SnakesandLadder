d2b442ca9e6864bb71b115f5912dc0fc493e4320
unknown <naveentruly86@gmail.com>
2021-05-20 18:43:11 +0530

    Added: Counting Dice Rolls

--- src/main/java/com/game/SnakenLadder.java
+++ src/main/java/com/game/SnakenLadder.java
@@ -6,18 +6,21 @@ public class SnakenLadder {
 
     public static void main(String[] args) {
 
+        // constants
         final int START_POSITION = 0;
         System.out.println("You are at position : " + START_POSITION);
         final int WINNING_POSITION = 100;
         final int NO_PLAY = 0;
         final int LADDER = 1;
         final int SNAKE = 2;
+        //Variables
         int position = 0;
-
+        int noOfdieRolls = 0;
         System.out.println("Rolling Dice" );
 
         while(position != WINNING_POSITION) {
 
+            noOfdieRolls++;
             Random o = new Random();
             int option = o.nextInt(3);
 
@@ -45,7 +48,8 @@ public class SnakenLadder {
                     break;
             }
             System.out.println("After Rolling dice You are at position : " + position);
-            System.out.println("HURRAY!,You are win");
         }
+        System.out.println("Number of times Die rolled : " + noOfdieRolls);
+        System.out.println("HURRAY!,You are win");
     }
 }
