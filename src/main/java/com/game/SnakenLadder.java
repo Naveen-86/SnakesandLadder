053b36a39e7775db80674c5c323bd09338ab4438
unknown <naveentruly86@gmail.com>
2021-05-20 17:09:44 +0530

    Added: Starting the Game

--- src/main/java/com/game/SnakenLadder.java
+++ src/main/java/com/game/SnakenLadder.java
@@ -3,5 +3,8 @@ package com.game;
 public class SnakenLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snakes and ladders game");
+
+        final int START_POSITION = 0;
+        System.out.println("You are at position : " + START_POSITION);
     }
 }
