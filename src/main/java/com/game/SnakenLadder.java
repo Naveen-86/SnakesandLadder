2d7fd271b65f915d694c1554559debf09565f0ef
Naveen Madhaboina <79349929+Naveen-86@users.noreply.github.com>
2021-05-24 08:40:39 +0530

    Update SnakenLadder.java

--- src/main/java/com/game/SnakenLadder.java
+++ src/main/java/com/game/SnakenLadder.java
@@ -1,10 +1,9 @@
 package com.game;
 
 public class SnakenLadder {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Snakes and ladders game");
+    	public static void main(String[] args){
 
-        final int START_POSITION = 0;
-        System.out.println("You are at position : " + START_POSITION);
-    }
+		int startPoint = 0;
+		int player1 = startPoint;
+	}
 }
